197639559b2159eb3550501a3f9da807136d3bfd
Jean Helou <jean.helou@gmail.com>
2012-12-17 13:34:32 +0100

    Migrate to play 2.1-RC1, version bump to 1.0.2

--- project/Build.scala
+++ project/Build.scala
@@ -1,22 +1,22 @@
 import sbt._
 import Keys._
-import PlayProject._
+import play._
 
 object ApplicationBuild extends Build {
 
   val appName = "api-ses"
-  val appVersion = "1.0.1"
+  val appVersion = "1.0.2"
 
   val appDependencies = Seq(
     "javax.mail" % "mail" % "1.4",
-    "nl.rhinofly" %% "library-utils" % "1.0.1")
+    "nl.rhinofly" %% "library-utils" % "1.0.2")
 
   def rhinoflyRepo(version: String) = {
     val repo = if (version endsWith "SNAPSHOT") "snapshot" else "release"
     Some("Rhinofly Internal " + repo.capitalize + " Repository" at "http://maven-repository.rhinofly.net:8081/artifactory/libs-" + repo + "-local")
   }
 
-  val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
+  val main = play.Project(appName, appVersion, appDependencies).settings(
     organization := "nl.rhinofly",
     resolvers += rhinoflyRepo("RELEASE").get,
     publishTo <<= version(rhinoflyRepo),
